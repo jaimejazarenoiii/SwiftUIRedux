ad81532f1c6a0b2427158d09e4daa24b75a83e18
= <jaimejazarenoiii@cpe-172-118-19-73.socal.res.rr.com>
2021-01-21 11:06:22 +0800

    Add project and task model

---
+++ SwiftUINewArchitecture/Models/Project.swift
@@ -0,0 +1,13 @@
+//
+//  Project.swift
+//  SwiftUINewArchitecture
+//
+//  Created by Jaime Jazareno III on 1/20/21.
+//
+
+import Foundation
+
+struct Project: Identifiable {
+    var id: Int
+    var title: String
+}
---
+++ SwiftUINewArchitecture/Models/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  SwiftUINewArchitecture
+//
+//  Created by Jaime Jazareno III on 1/20/21.
+//
+
+import Foundation
+
+struct Task: Identifiable {
+    var id: Int
+    var title: String
+    var description: String
+}
