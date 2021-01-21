d01c5105d87b61fd5c03a02a94702a1c5ef0bed0
= <jaimejazarenoiii@cpe-172-118-19-73.socal.res.rr.com>
2021-01-21 11:08:09 +0800

    Add global world

---
+++ SwiftUINewArchitecture/Store/World.swift
@@ -0,0 +1,14 @@
+//
+//  App.swift
+//  SwiftUINewArchitecture
+//
+//  Created by Jaime Jazareno III on 1/20/21.
+//
+
+import Foundation
+import Combine
+
+// For more information check "How To Control The World" - Stephen Celis
+// https://vimeo.com/291588126
+struct World {
+}
